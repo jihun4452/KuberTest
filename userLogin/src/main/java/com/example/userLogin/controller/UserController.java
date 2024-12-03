d1ad1b4b82a71f41a84aaf78e32a10b5e6507abb
taenam kim <ktn2674@naver.com>
2024-12-03 16:14:35 +0900

    feat:UserController add

--- userLogin/src/main/java/com/example/userLogin/controller/UserController.java
+++ userLogin/src/main/java/com/example/userLogin/controller/UserController.java
@@ -1,12 +1,31 @@
 package com.example.userLogin.controller;
 
+import com.example.userLogin.dto.user.request.UserLoginRequestDto;
+import com.example.userLogin.dto.user.request.UserSignupRequestDto;
+import com.example.userLogin.service.UserServiceImpl;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
-
 public class UserController {
 
+  private final UserServiceImpl userService;
+
+  @PostMapping("/signup")
+  public ResponseEntity<String> signup(@RequestParam UserSignupRequestDto userSignupRequestDto, HttpServletResponse response) {
+    userService.signUp(userSignupRequestDto,response);
+    return ResponseEntity.ok("Signup Successful");
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<String> login(@RequestParam UserLoginRequestDto userLoginRequestDto, HttpServletResponse response) {
+    userService.login(userLoginRequestDto,response);
+    return ResponseEntity.ok("Login Successful");
+  }
 
 }
